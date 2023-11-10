50bc4798c333da3b78d25f2f5cb0b3f2edd26e4a
zhaomingyu <zhaomingyu@meituan.com>
2023-11-10 21:07:37 +0800

    init

---
+++ wxapp-server-api/src/main/java/com/myz/wxapp/wxappserver/dubbo/api/DemoService.java
@@ -0,0 +1,7 @@
+package com.myz.wxapp.wxappserver.dubbo.api;
+
+public interface DemoService {
+
+    String sayHello(String name);
+
+}
---
+++ wxapp-server-service/src/test/java/com/myz/wxapp/wxappserver/WxappServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myz.wxapp.wxappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxappServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
