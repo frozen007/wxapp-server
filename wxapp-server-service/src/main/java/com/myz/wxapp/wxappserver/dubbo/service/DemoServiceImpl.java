cc81065aa0f30293892cf1e64e0bbed51175e9de
zhaomingyu <zhaomingyu@meituan.com>
2023-11-14 14:05:41 +0800

    upgrade spring-boot to 3.1.5

--- wxapp-server-service/src/main/java/com/myz/wxapp/wxappserver/dubbo/service/DemoServiceImpl.java
+++ wxapp-server-service/src/main/java/com/myz/wxapp/wxappserver/dubbo/service/DemoServiceImpl.java
@@ -3,12 +3,17 @@ package com.myz.wxapp.wxappserver.dubbo.service;
 import com.myz.wxapp.wxappserver.dubbo.api.DemoService;
 
 import org.apache.dubbo.config.annotation.DubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @DubboService
 public class DemoServiceImpl implements DemoService {
 
+    Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
+
     @Override
     public String sayHello(String name) {
+        logger.info("invoked name=" + name);
         return "Hello " + name;
     }
 }
